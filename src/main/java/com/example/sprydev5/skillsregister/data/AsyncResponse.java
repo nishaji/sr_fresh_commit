412b9b89f097d7506dbeabda1642dabfcbc9b39a
spry_devteam02 <devteam02@sprytechies.com>
2016-09-21 15:56:42 +0530

    remove old files

--- src/main/java/com/example/sprydev5/skillsregister/data/AsyncResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.sprydev5.skillsregister.data;
-
-/**
- * Created by pradeep on 4/11/15.
- */
-public interface AsyncResponse {
-    void ProcessResponse(String path, String result);
-}
