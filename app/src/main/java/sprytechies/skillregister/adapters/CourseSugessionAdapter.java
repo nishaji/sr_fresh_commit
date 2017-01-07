68c7f92702d7ff6352db95c4fa55802a6d819b6f
spry_devteam02 <devteam02@sprytechies.com>
2017-01-07 18:24:53 +0530

    removed app folder

--- app/src/main/java/sprytechies/skillregister/adapters/CourseSugessionAdapter.java
+++
@@ -1,8 +0,0 @@
-package sprytechies.skillregister.adapters;
-
-/**
- * Created by sprydev5 on 22/9/16.
- */
-
-public class CourseSugessionAdapter {
-}
--- app/src/main/java/sprytechies/skillregister/apicallclasses/AsyncResponse.java
+++
@@ -1,7 +0,0 @@
-package sprytechies.skillregister.apicallclasses;
-/**
- * Created by pradeep on 4/11/15.
- */
-public interface AsyncResponse {
-    void ProcessResponse(String path, String result);
-}
